908cf0eb67a5cfd9e359af3720e58b15f111f100
Ian McGregor <ian@stinkdigital.com>
2015-05-01 10:54:06 +0100

    fixes issue with recorder util in chrome 42

--- src/lib/effect/recorder.js
+++ src/lib/effect/recorder.js
@@ -1,18 +1,15 @@
 'use strict';
 
 function Recorder(context, passThrough) {
-    var buffersL = [],
+    var bufferLength = 4096,
+        buffersL = [],
         buffersR = [],
         startedAt = 0,
         stoppedAt = 0;
 
     var input = context.createGain();
     var output = context.createGain();
-    var script = context.createScriptProcessor(4096, 2, 2);
-    
-    input.connect(script);
-    script.connect(context.destination);
-    script.connect(output);
+    var script;
 
     var node = input;
     node.name = 'Recorder';
@@ -22,15 +19,61 @@ function Recorder(context, passThrough) {
 
     var getBuffer = function() {
         if(!buffersL.length) {
-            return context.createBuffer(2, 4096, context.sampleRate);
+            return context.createBuffer(2, bufferLength, context.sampleRate);
         }
-        var buffer = context.createBuffer(2, buffersL.length, context.sampleRate);
-        buffer.getChannelData(0).set(buffersL);
-        buffer.getChannelData(1).set(buffersR);
+        var recordingLength = buffersL.length * bufferLength;
+        var buffer = context.createBuffer(2, recordingLength, context.sampleRate);
+        buffer.getChannelData(0).set(mergeBuffers(buffersL, recordingLength));
+        buffer.getChannelData(1).set(mergeBuffers(buffersR, recordingLength));
         return buffer;
     };
 
+    var mergeBuffers = function(buffers, length) {
+        var buffer = new Float32Array(length);
+        var offset = 0;
+        for (var i = 0; i < buffers.length; i++) {
+          buffer.set(buffers[i], offset);
+          offset += buffers[i].length;
+        }
+        return buffer;
+    };
+
+    var createScriptProcessor = function() {
+      destroyScriptProcessor();
+
+      script = context.createScriptProcessor(bufferLength, 2, 2);
+      input.connect(script);
+      script.connect(context.destination);
+      script.connect(output);
+
+      script.onaudioprocess = function (event) {
+          var inputL = event.inputBuffer.getChannelData(0),
+              inputR = event.inputBuffer.getChannelData(1);
+
+          if(passThrough) {
+              var outputL = event.outputBuffer.getChannelData(0),
+                  outputR = event.outputBuffer.getChannelData(1);
+              outputL.set(inputL);
+              outputR.set(inputR);
+          }
+
+          if(node.isRecording) {
+              buffersL.push(new Float32Array(inputL));
+              buffersR.push(new Float32Array(inputR));
+          }
+      };
+    };
+
+    var destroyScriptProcessor = function() {
+      if (script) {
+        script.onaudioprocess = null;
+        input.disconnect();
+        script.disconnect();
+      }
+    };
+
     node.start = function() {
+        createScriptProcessor();
         buffersL.length = 0;
         buffersR.length = 0;
         startedAt = context.currentTime;
@@ -41,6 +84,7 @@ function Recorder(context, passThrough) {
     node.stop = function() {
         stoppedAt = context.currentTime;
         this.isRecording = false;
+        destroyScriptProcessor();
         return getBuffer();
     };
 
@@ -49,25 +93,6 @@ function Recorder(context, passThrough) {
             return stoppedAt - startedAt;
         }
         return context.currentTime - startedAt;
-    };
-
-    script.onaudioprocess = function (event) {
-        var inputL = event.inputBuffer.getChannelData(0),
-            inputR = event.inputBuffer.getChannelData(0),
-            outputL = event.outputBuffer.getChannelData(0),
-            outputR = event.outputBuffer.getChannelData(0);
-
-        if(passThrough) {
-            outputL.set(inputL);
-            outputR.set(inputR);
-        }
-
-        if(node.isRecording) {
-            for (var i = 0; i < inputL.length; i++) {
-                buffersL.push(inputL[i]);
-                buffersR.push(inputR[i]);
-            }
-        }
     };
 
     return node;
